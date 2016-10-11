d0dd80376041e3301270e7ce7e5d58e084b980f1
Andrew Ayers <andrewa@ecreations.net>
2016-10-11 16:59:32 -0700

    Update to something that is 'working' - but still isn't fully correct. array structures aren't matching wanted layout (see comments). This needs to be fixed. Sigh.

--- requirejs/lin-alg/www/app/main.js
+++ requirejs/lin-alg/www/app/main.js
@@ -2,12 +2,31 @@ define(function (require) {
     var print = require('common/print');
     var lina = require('math/lin-alg');
 
-    print(lina.hello());
+    console.log(lina.hello());
 
-    var v1 = lina.rowV( [ 1, 2, 3 ] );
-    var v2 = lina.colV( [ 2, 4, 6 ] );
+    // Y x X
+    var sca1 = lina.scalar(1);
+    var mat1 = lina.matrix([1, 2, 3], 3, 1); // [[ 1, 2, 3 ]] => 3 x 1
+    var mat2 = lina.matrix([1, 2, 3], 1, 3); // [[ 1 ], [ 2 ], [ 3 ]] => 1 x 3
+    var mat3 = lina.matrix([1, 2, 3, 4], 2, 2); // [[ 1, 2 ], [ 3, 4 ]] => 2 x 2
+    var mat4 = lina.matrix([1, 2, 3, 4, 5, 6], 3, 2); // [[ 1, 3, 5 ], [ 2, 4, 6 ] ] => 3 x 2
+    var mat5 = lina.matrix([1, 2, 3, 4, 5, 6], 2, 3); // [[ 1, 4 ], [ 2, 5 ], [ 3, 6 ] ] => 2 x 3
+    var mat6 = lina.matrix([1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3); // [[ 1, 4, 7 ], [ 2, 5, 8 ], [ 3, 6, 9 ]] => 3 x 3
 
-    var res = lina.multiply(v1, v2);
+    console.log('scalar', sca1);
+    console.log('3 x 1', mat1);
+    console.log('1 x 3', mat2);
+    console.log('2 x 2', mat3);
+    console.log('3 x 2', mat4);
+    console.log('2 x 3', mat5);
+    console.log('3 x 3', mat6);
 
-    print(res);
+    var matA = mat5;
+    console.log('matA - 2 x 3', matA);
+
+    var matB = lina.matrix([7, 8, 9, 10, 11, 12], 3, 2); // [[ 7, 9, 11 ], [ 8, 10, 12 ]] => 3 x 2
+    console.log('matB - 3 x 2', matB);
+
+    var res = lina.dot(matA, matB);
+    console.log('dot-product', res);
 });
--- requirejs/lin-alg/www/lib/math/lin-alg.js
+++ requirejs/lin-alg/www/lib/math/lin-alg.js
@@ -6,26 +6,66 @@ define(function (require) {
             return 'Welcome to the Linear Algebra Library Test-Bed!';
         },
 
-        rowV: function (v) {
-            return v.slice();
+        size: function (mat){
+            if (mat instanceof Array) {
+                return [mat.length].concat(this.size(mat[0]));///.reverse();
+            } else {
+                return [];
+            }
         },
 
-        colV: function (v) {
-            return new Array(v);
+        scalar: function (n) {
+            return {
+                value: n,
+                type: 'scalar'
+            };
         },
 
-        multiply: function (vec1, vec2) {
-            print(vec1);
-            print(vec2);
+        matrix: function (values, rows, cols) {
+            if (values instanceof Array) {
+                if (values.length == (rows * cols)) {
+                    var arr = [];
 
-            if (vec1.length > vec2.length) {
-                return 'dot';
+                    for (var j = 0; j < rows; j++) {
+                        var row = [];
+                        for (var i = 0; i < cols; i++) {
+                            row[i] = values[i + (cols * j)];
+                        }
+                        arr[j] = row;
+                    }
+
+                    return {
+                        value: arr,
+                        type: 'matrix',
+                        size: this.size(arr)
+                    };
+                }
             }
-            else if (vec2.length > vec1.length) {
-                return 'tensor';
-            }
-            else {
-                return 'square';
+        },
+
+        dot: function (a, b) {
+            if (a.type === 'matrix' && b.type === 'matrix') {
+                if (a.size[1] === b.size[0]) {
+                    var rows = a.size[0];
+                    var cols = b.size[1];
+
+                    var arr = [];
+                    for (var j = 0; j < rows; j++) {
+                        arr[j] = [];
+                        for (var i = 0; i < cols; i++) {
+                            arr[j][i] = 0;
+                            for (var v = 0; v < a.size[1]; v++) {
+                                arr[j][i] += (a.value[j][v] * b.value[v][i]);
+                            }
+                        }
+                    }
+
+                    return {
+                        value: arr,
+                        type: 'matrix',
+                        size: this.size(arr)
+                    };
+                }
             }
         }
     };
